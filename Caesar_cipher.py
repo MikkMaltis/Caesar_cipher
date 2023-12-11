e9d70a13b1d18bee0174b2a0daf283f866e840a1
MikkMaltis <maltismikk@gmail.com>
2023-12-11 15:26:48 +0200

    .

--- Caesar_cipher.py
+++ Caesar_cipher.py
@@ -1,5 +1,6 @@
 chars = ['a','b','c','d', 'e','f','g','h','i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z']
 
+# encrypting
 text = input("Enter the text: ")
 key = int(input("Enter the key: "))
 result = []
@@ -11,3 +12,18 @@ for char in list(text):
         result.append(char)
 
     print(result)
+
+# decrypting
+text = input("Enter the text: ")
+key = int(input("Enter the key: "))
+result = []
+
+for char in list(text):
+    if char in chars:
+        decrypted_char = chars[(chars.index(char) - key) % len(chars)]
+        result.append(decrypted_char)
+    else:
+        result.append(char)
+
+decrypted_text = ''.join(result)
+print("Decrypted text:", decrypted_text)
